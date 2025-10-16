34aab6d8c11e6b02b42502b394052172ea91cb5e
Christopher Alston <caa22c@fsu.edu>
2025-10-16 15:39:11 -0400

    Adds functionality to change current resolution

--- DungeonJanitorCEN4090L/Assets/Scripts/SettingsMenu.cs
+++ DungeonJanitorCEN4090L/Assets/Scripts/SettingsMenu.cs
@@ -1,16 +1,61 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using System.Collections.Generic;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    public void SetVolume (float volume)
+    public TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+       
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+    }
+
+    public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
     }
 
-    public void SetQuality (int qualityIndex)
+
+    public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        bool isFullscreen = Screen.fullScreen;
+        Screen.SetResolution(resolution.width, resolution.height, isFullscreen);
     }
 }
