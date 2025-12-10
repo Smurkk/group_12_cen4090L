1055dfbe69abe7efe433098b506a038ce19756cf
gusfontenot <gusfontenot90@gmail.com>
2025-12-10 10:25:14 -0500

    Created temporary EnemyRespawnManger file for compilation

---
+++ DungeonJanitorCEN4090L/Assets/Scripts/EnemyRespawnManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EnemyRespawnManager : MonoBehaviour
+{
+    // temporary placeholder so the project can compile
+
+    public void OnEnemyDied(Enemy enemy)
+    {
+        // TODO: real logic will go here when teammate implements it.
+        // For now, do nothing.
+    }
+}
